6ec03cde1d3b41088e687cae8b845f4b8a881238
mrplug1n <42859835+mrplug1n@users.noreply.github.com>
2018-09-08 16:30:09 +0800

    Create SpamOnline.sh

---
+++ SpamOnline.sh
@@ -0,0 +1,111 @@
+#!/system/xbin/bash
+clear
+figlet KCG | lolcat
+toilet -f slant --gay "SpamOnline"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: Kaneki Cyber Ghost"
+sleep 1
+echo "\033[32;1m contact : +6281775141230"
+sleep 1
+echo "\033[34;1m Kaneki Cyber Ghost"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Tokopedia"
+echo "2. Telkomnyet"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "0. Keluar"
+echo "\033[30;1m81. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read KCG
+if [ $KCG = 1 ] || [ $KCG = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+fi
+
+if
+[ $KCG = 2 ] || [ $KCG = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $KCG = 3 ] || [ $KCG = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+
+if [ $KCG = 4 ] || [ $KCG = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if
+[ $KCG = 5 ] || [ $KCG = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+fi
+
+if
+[ $KCG = 6 ] || [ $KCG = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $KCG = 81 ] || [ $KCG = 81 ]
+then
+clear
+toilet -f slant --gay "SpamOnline"
+echo "\033[31;1mNama tools: SpamOnline"
+sleep 1
+echo "\033[33;1mKarya: Mr.OneJack"
+sleep 1
+echo "\033[32;1mVersi: v2"
+sleep 1
+echo "\033[36;1mTeam: Kaneki Cyber Ghost"
+sleep 1
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "Mr.plug1n"
+echo "MR.4Z13X"
+echo "MR.Y05H"
+echo "MR.RANSX"
+echo "MR.W1LD"
+sleep 1
+echo "And All Member Kaneki Cyber Ghost"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh SpamOnline.sh
+fi
+
+if
+[ $KCG = 0 ] || [ $KCG = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Dadah Kentod :)"
+sleep 1
+fi
